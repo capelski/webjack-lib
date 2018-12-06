6b462b6396e8c43d3a5db9696ff89f071cd940a8
Carles Capellas Careta <capellas.carles@gmail.com>
2018-12-06 19:59:11 +0100

    Remove unsused generic methods

--- src/utils/js-generics.ts
+++ src/utils/js-generics.ts
@@ -1,5 +1,3 @@
-'use strict';
-
 const cartesianProduct = <T, U>(firstArray: T[], secondArray: U[], elementBuilder: (t: T, u: U) => any) => {
     return [].concat.apply([], firstArray.map(function (x) {
         return [].concat.apply([], secondArray.map(function (y) {
@@ -8,80 +6,10 @@ const cartesianProduct = <T, U>(firstArray: T[], secondArray: U[], elementBuilde
     }));
 };
 
-const clone = (source: any) => {
-    if (typeof(source) !== typeof({})) {
-        return source;
-    }
-    var newObject = source instanceof Array ? [] : {} as {[key: string]: any};
-    iterate(source, (element: any, key: string) => {
-        if (element instanceof Array || typeof(element) === typeof({})) {
-            newObject[key] = clone(source[key]);
-        } else {
-            newObject[key] = source[key];
-        }                
-    });
-    return newObject;
-};
-
-const createArray = (size: number, elementBuilder: Function) => {
-    var array: any[] = [];
-    repeat(size, (iterationNumber: number) => {
-        var newArray = elementBuilder(array, iterationNumber - 1);
-        if (newArray) {
-            array = newArray;
-        }
-    });
-    return array;
-};
-
-const iterate = (iterable: any[], functionExpression: Function) => {
-    for (var key in iterable) {
-        functionExpression(iterable[key], key);
-    }
-};
-
-const iterateFor = (array: any[], functionExpression: Function) => {
-    for (var i = 0; i < array.length; ++i) {
-        functionExpression(array[i], i);
-    }
-};
-
-const numericalRange = (lowerBoundary: number, upperBoundary: number) => {
-    let range = [];
-    for (var i = lowerBoundary; i <= upperBoundary; ++i) {
-        range.push(i);
-    }
-    return range;
-};
-
-const percentizeValues = () => {
-    var total = 0,
-    index;            
-    for (index in arguments) {
-        total += arguments[index];
-    }
-    var values = [];
-    for (index in arguments) {
-        var value = arguments[index];
-        var percentizedValue = 0;
-        if (total > 0) {
-            percentizedValue = Math.round(value * 10000 / total) / 100;
-        }
-        values.push(percentizedValue);
-    }
-    return values;
-};
-
 const removeDuplicates = (numbers: number[]): number[] => {
     const numbersDictionary: { [key: string]: number } =
         numbers.reduce((dictionary, next) => ({...dictionary, [next]: next}), {});
     return Object.keys(numbersDictionary).map(x => numbersDictionary[x]);
-};
-
-const repeat = (iterationsNumber: number, functionExpression: Function) => {
-    for (var i = 1; i < (iterationsNumber + 1); ++i) {
-        functionExpression(i);
-    }
 };
 
 const shuffleArray = (array: any[]) => {
@@ -95,26 +23,12 @@ const shuffleArray = (array: any[]) => {
 
 export {
     cartesianProduct,
-    iterate,
-    clone,
     removeDuplicates,
-    repeat,
-    createArray,
-    iterateFor,
-    numericalRange,
-    percentizeValues,
     shuffleArray
 };
 
 export default {
     cartesianProduct,
-    iterate,
-    clone,
     removeDuplicates,
-    repeat,
-    createArray,
-    iterateFor,
-    numericalRange,
-    percentizeValues,
     shuffleArray
 };
