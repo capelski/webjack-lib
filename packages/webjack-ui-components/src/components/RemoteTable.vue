b50c50b83913cc92135e4c52067e991ed2c4e42f
Carles Capellas <capellas.carles@gmail.com>
2019-05-26 17:50:33 +0200

    Support serverUrl templates along with base urls

--- packages/webjack-ui-components/src/components/RemoteTable.vue
+++ packages/webjack-ui-components/src/components/RemoteTable.vue
@@ -11,7 +11,7 @@
             :displayDecisionHelp="true"
             :startRoundButtonText="'Place bet'"
         />
-        <Loader v-if="loading" />
+        <Loader v-if="loading"/>
     </div>
 </template>
 
@@ -24,6 +24,12 @@
     import { stallPromise } from '../utils/shared';
 
     declare const toastr: any;
+
+    const getEndpointUrl = (serverUrl: string, endpoint: string) => {
+        return serverUrl.indexOf('{endpoint}') > -1 ?
+            serverUrl.replace('{endpoint}', endpoint) : 
+            serverUrl + `/${endpoint}`;
+    };
 
     export default {
         name: 'RemoteTable',
@@ -54,7 +60,7 @@
         },
         created() {
             const updateTableInterval = () => get(
-                this.serverUrl + '/table-status',
+                getEndpointUrl(this.serverUrl, 'table-status'),
                 null,
                 { message: true },
                 'Error getting the table status')
@@ -71,7 +77,7 @@
             let tablePromise = Promise.resolve(this.tableId);
             if (!this.tableId) {
                 tablePromise = get(
-                    this.serverUrl + '/join-table',
+                    getEndpointUrl(this.serverUrl, 'join-table'),
                     null,
                     {},
                     'Error trying to join a table. Please refresh the screen and try again')
@@ -99,65 +105,86 @@
         },
         methods: {
             double() {
-                get(this.serverUrl + '/make-decision', { decision: PlayerActions.Double }, null, 'Error on double');
+                get(
+                    getEndpointUrl(this.serverUrl, 'make-decision'),
+                    { decision: PlayerActions.Double },
+                    null,
+                    'Error on double');
             },
             exitTable() {
                 this.loading = true;
                 clearInterval(this.tableInterval!);
-                stallPromise(get(this.serverUrl + '/exit-table')).then(() => this.$emit('TableExited'));
+                stallPromise(get(getEndpointUrl(this.serverUrl, 'exit-table')))
+                    .then(() => this.$emit('TableExited'));
             },
             hit() {
-                get(this.serverUrl + '/make-decision', { decision: PlayerActions.Hit }, null, 'Error on hit');
+                get(
+                    getEndpointUrl(this.serverUrl, 'make-decision'),
+                    { decision: PlayerActions.Hit },
+                    null,
+                    'Error on hit');
             },
             isUserPlayer(player: Player) {
                 return player && player.id === this.userId;
             },
             split() {
-                get(this.serverUrl + '/make-decision', { decision: PlayerActions.Split }, null, 'Error on split');
+                get(
+                    getEndpointUrl(this.serverUrl, 'make-decision'),
+                    { decision: PlayerActions.Split },
+                    null,
+                    'Error on split');
             },
             stand() {
-                get(this.serverUrl + '/make-decision', { decision: PlayerActions.Stand }, null, 'Error on stand');
+                get(
+                    getEndpointUrl(this.serverUrl, 'make-decision'),
+                    { decision: PlayerActions.Stand },
+                    null,
+                    'Error on stand');
             },
             startRound() {
                 const bet = 1;
-                get(this.serverUrl + '/place-bet', { bet }, null, 'Error placing the bet');
+                get(
+                    getEndpointUrl(this.serverUrl, 'place-bet'),
+                    { bet },
+                    null,
+                    'Error placing the bet');
             }
         }
     };
 </script>
 
 <style>
-    .black-jack-table {
-        display: flex;
-        flex-direction: column;
-        height: calc(100% - 89px);
-        overflow: hidden;
-    }
+.black-jack-table {
+  display: flex;
+  flex-direction: column;
+  height: calc(100% - 89px);
+  overflow: hidden;
+}
 
-    .table-dealer {
-        margin-top: 20px;
-    }
-    @media(min-width: 992px) {
-        .table-dealer {
-            width: 300px;
-            margin: 0 auto;
-            margin-top: 20px;
-            text-align: center;
-        }
-    }
+.table-dealer {
+  margin-top: 20px;
+}
+@media (min-width: 992px) {
+  .table-dealer {
+    width: 300px;
+    margin: 0 auto;
+    margin-top: 20px;
+    text-align: center;
+  }
+}
 
-    .table-players {
-        display: flex;
-        flex-direction: column;
-        margin-top: 20px;
-        overflow-y: auto;
-        overflow-x: hidden;
-    }
-    
-    @media(min-width: 992px) {
-        .table-players {
-            margin-top: 0;
-            flex-direction: row;
-        }
-    }
+.table-players {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+@media (min-width: 992px) {
+  .table-players {
+    margin-top: 0;
+    flex-direction: row;
+  }
+}
 </style>
